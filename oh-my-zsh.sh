3f5cce721020347e55365a74c96c48c4aa6cbae8
pavekovt <39086668+pavekovt@users.noreply.github.com>
2018-06-18 22:37:58 +0100

    Initial commit

---
+++ oh-my-zsh.sh
@@ -0,0 +1,14 @@
+#install zsh
+sudo apt-get install zsh
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+#
+
+#tmux
+sudo apt-get install -y tmux
+#
