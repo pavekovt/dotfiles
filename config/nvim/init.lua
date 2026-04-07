d76c7b2584272db554224f9a8089cfe0d0fcdca2
pavekovt <pavels.kovtuns@gmail.com>
2026-04-07 14:01:04 +0300

    yank hightlight

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,3 +2,11 @@ require("pavekovt")
 
 vim.o.scrolloff = 10
 vim.o.confirm = true
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
